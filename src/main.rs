ba286456af3d5439d721b41e14a6d24ae6fcf949
yavko <yavornkolev@gmail.com>
2024-02-12 22:10:38 -0800

    first rat

---
+++ src/main.rs
@@ -0,0 +1,127 @@
+use anyhow::Result;
+use bpaf::*;
+use rodio::{Decoder, OutputStream, Sink};
+use std::io::Cursor;
+mod frames;
+mod rainbow;
+
+use std::sync::{
+    atomic::{AtomicU64, Ordering},
+    OnceLock,
+};
+#[derive(Default, Debug, Clone, Bpaf)]
+#[bpaf(options)]
+struct Options {
+    /// Enable rainbow mode
+    #[bpaf(short, long)]
+    rainbow: bool,
+    /// Enable ratmark
+    #[bpaf(short('m'), long)]
+    ratmark: bool,
+    /// Enable framerate unlock
+    #[bpaf(short('u'), long("unlock"))]
+    fps_unlock: bool,
+    /// Enable debug mode
+    #[bpaf(short, long)]
+    debug: bool,
+}
+
+static LOOP_COUNT: AtomicU64 = AtomicU64::new(0);
+
+static OPTIONS: OnceLock<Options> = OnceLock::new();
+
+fn get_options() -> Options {
+    OPTIONS.get_or_init(|| options().run()).clone()
+}
+
+fn print_debug() {
+    let opts = get_options();
+    if opts.debug {
+        if opts.rainbow {
+            print!("Rainbow enabled\n");
+        }
+        if opts.ratmark {
+            print!("Ratmark enabled\n");
+        }
+        if opts.fps_unlock {
+            print!("Framerate unlock enabled\n");
+        }
+        print!("Loop count: {}\n", LOOP_COUNT.load(Ordering::SeqCst));
+    }
+}
+
+fn port_cls() {
+    print!("\x1b[H\033[J");
+}
+
+fn port_gotoxy(x: i32, y: i32) {
+    print!("\x1b[{y};{x}H");
+}
+const AUDIO: &[u8] = include_bytes!("./audio_loop.wav");
+
+fn main() -> Result<()> {
+    use std::time::Instant;
+    let start = Instant::now();
+    // static struct option long_options[] = {
+    //     {"rainbow", no_argument, 0, 'r'},
+    //     {"ratmark", no_argument, 0, 'm'},
+    //     {"unlock", no_argument, 0, 'u'},
+    //     {"debug", no_argument, 0, 'd'},
+    //     {"help", no_argument, 0, 'h'},
+    //     {0, 0, 0, 0}
+    // };
+    let opts = get_options();
+    // if opts.rainbow {
+    //     eprintln!("Rainbow mode not supported");
+    //     std::process::exit(1);
+    // }
+
+    port_cls();
+    port_gotoxy(0, 0);
+
+    std::thread::spawn(|| audio().unwrap());
+
+    let mut index = 0;
+    loop {
+        print_debug();
+        if !opts.rainbow {
+            print!("\n                                                         You have been blessed with {} spins of the rat.\n", LOOP_COUNT.load(Ordering::SeqCst));
+            print!("{}", frames::FRAMES[index]);
+        } else {
+            use rainbow::print_rainbow;
+            print_rainbow!("\n                                                         You have been blessed with {} spins of the rat.\n", LOOP_COUNT.load(Ordering::SeqCst));
+            print_rainbow!("{}", frames::FRAMES[index]);
+        }
+        if !opts.fps_unlock && !opts.ratmark {
+            std::thread::sleep(std::time::Duration::from_millis(frames::DELAY_TIME as u64))
+        }
+
+        if index == (frames::FRAME_COUNT - 2) as usize {
+            if LOOP_COUNT.load(Ordering::SeqCst) == 100 && opts.ratmark {
+                let end = Instant::now();
+                let time_taken = start - end;
+                print!("100 ratmarks in {}μs\n", time_taken.as_micros());
+                return Ok(());
+            };
+
+            LOOP_COUNT.fetch_add(1, Ordering::SeqCst);
+            index = 0;
+        };
+
+        port_gotoxy(0, 0);
+        index += 1;
+    }
+}
+fn audio() -> Result<()> {
+    loop {
+        // Get a output stream handle to the default physical sound device
+        let (_stream, stream_handle) = OutputStream::try_default()?;
+        let sink = Sink::try_new(&stream_handle)?;
+        // Decode that sound file into a source
+        let source = Decoder::new_wav(Cursor::new(AUDIO))?;
+        // Play the sound directly on the device
+        sink.append(source);
+
+        sink.sleep_until_end();
+    }
+}
---
+++ src/rainbow.rs
@@ -0,0 +1,35 @@
+// code borrowed from https://github.com/BlueTufa/lolcat/blob/master/src/main.rs
+use ansi_term::Colour;
+use rand::Rng;
+
+fn rainbow(seed: f64) -> ansi_term::Colour {
+    let freq = 1.13;
+    let factor = seed / 16.0;
+
+    let red = (freq * factor as f64).sin().mul_add(127.0, 128.0) as u8;
+    let green = (freq * factor + 2.0 * std::f64::consts::PI / 3.0 as f64)
+        .sin()
+        .mul_add(127.0, 128.0) as u8;
+    let blue = (freq * factor + 4.0 * std::f64::consts::PI / 3.0 as f64)
+        .sin()
+        .mul_add(127.0, 128.0) as u8;
+
+    Colour::RGB(red, green, blue)
+}
+
+pub fn print_rainbow(string: String) {
+    let entropy = rand::thread_rng().gen_range(0..=255);
+    for (ix, c) in string.char_indices() {
+        let seed = f64::from(ix as u32 + entropy);
+        let colour = rainbow(seed);
+        let rainbow_char = colour.paint(c.to_string());
+        print!("{}", rainbow_char);
+    }
+}
+
+#[macro_export]
+macro_rules! print_rainbow {
+    ($($arg:tt)*) => {{
+        $crate::rainbow::print_rainbow(format!($($arg)*))
+    }};
+}
